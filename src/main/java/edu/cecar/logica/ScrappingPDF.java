7b5eff9d19589d1be11ac23edaffbbd1bc0f9757
oderb <oderb@DESKTOP-5G4OEVQ>
2020-10-02 16:35:40 -0500

    se creo un objeto de tipo CasosCovid para manejar los datos de los pdf. Se comenzo a extraer los datos de los pdf y a tratar estos datos.

--- src/main/java/edu/cecar/logica/ScrappingPDF.java
+++ src/main/java/edu/cecar/logica/ScrappingPDF.java
@@ -5,12 +5,45 @@
  */
 package edu.cecar.logica;
 
+import com.itextpdf.awt.geom.Rectangle;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.parser.FilteredTextRenderListener;
+import com.itextpdf.text.pdf.parser.LocationTextExtractionStrategy;
+import com.itextpdf.text.pdf.parser.PdfTextExtractor;
+import com.itextpdf.text.pdf.parser.RegionTextRenderFilter;
+import com.itextpdf.text.pdf.parser.RenderFilter;
+import com.itextpdf.text.pdf.parser.TextExtractionStrategy;
+import java.io.IOException;
+
 /**
  *
  * @author oderb
  */
 public class ScrappingPDF {
+
+    //C:\Users\oderb\Documents\NetBeansProjects\ReconocedorRelaciones\anexos
     
-    
-    
+    public static String lectorPDF(String ruta) throws IOException {
+
+        PdfReader lector = new PdfReader(ruta);
+
+        Rectangle rect = new Rectangle(0, 120, 500, 230);
+
+        RenderFilter filtro = new RegionTextRenderFilter(rect);
+
+        TextExtractionStrategy strategy;
+
+        strategy = new FilteredTextRenderListener(new LocationTextExtractionStrategy(), filtro);
+
+        String contenido = PdfTextExtractor.getTextFromPage(lector, 9, strategy);
+
+        return contenido;
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(lectorPDF("./anexos/20200812-covid-19-sitrep-205.pdf"));
+        
+    }
+
 }
---
+++ src/main/java/edu/cecar/persistencia/CasosCovid.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.cecar.persistencia;
+
+/**
+ *
+ * @author oderb
+ */
+public class CasosCovid {
+    
+    int totalCasosConfirmados;
+    int totalCasosNuevosConfirmados;
+    int totalMuertos;
+    int TotalNuevosMuertos;
+
+    public CasosCovid() {
+    }
+
+    public CasosCovid(int totalCasosConfirmados, int totalCasosNuevosConfirmados, int totalMuertos, int TotalNuevosMuertos) {
+        this.totalCasosConfirmados = totalCasosConfirmados;
+        this.totalCasosNuevosConfirmados = totalCasosNuevosConfirmados;
+        this.totalMuertos = totalMuertos;
+        this.TotalNuevosMuertos = TotalNuevosMuertos;
+    }
+
+    public int getTotalCasosConfirmados() {
+        return totalCasosConfirmados;
+    }
+
+    public void setTotalCasosConfirmados(int totalCasosConfirmados) {
+        this.totalCasosConfirmados = totalCasosConfirmados;
+    }
+
+    public int getTotalCasosNuevosConfirmados() {
+        return totalCasosNuevosConfirmados;
+    }
+
+    public void setTotalCasosNuevosConfirmados(int totalCasosNuevosConfirmados) {
+        this.totalCasosNuevosConfirmados = totalCasosNuevosConfirmados;
+    }
+
+    public int getTotalMuertos() {
+        return totalMuertos;
+    }
+
+    public void setTotalMuertos(int totalMuertos) {
+        this.totalMuertos = totalMuertos;
+    }
+
+    public int getTotalNuevosMuertos() {
+        return TotalNuevosMuertos;
+    }
+
+    public void setTotalNuevosMuertos(int TotalNuevosMuertos) {
+        this.TotalNuevosMuertos = TotalNuevosMuertos;
+    }
+    
+    
+
+    
+}
